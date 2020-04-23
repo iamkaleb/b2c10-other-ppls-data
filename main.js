5ed411f54177f353afa9904dd41a55d4743b004c
iamkaleb <kalebdmoran@gmail.com>
2020-04-23 14:16:50 -0500

    Added ingredients fetch from Open Food Facts

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const createFoodComponent = (food) => {
             <ul class="foodList__list">
                 <li class="foodList__item--nopadding">${food.category}</li>
                 <li class="foodList__item--nopadding">${food.ethnicity}</li>
+                <li class="foodList__item--nopadding">${food.ingredients}</li>
             </ul>
         </section>
     `
@@ -12,12 +13,30 @@ const createFoodComponent = (food) => {
 
 const foodContainer = document.querySelector(".foodList")
 
-fetch("http://localhost:8088/food")
-    .then(foods => foods.json())
-    .then(parsedFoods => {
-        parsedFoods.forEach(food => {
-            foodComponent = createFoodComponent(food)
-            foodContainer.innerHTML += foodComponent
+// fetch("http://localhost:8088/food")
+//     .then(foods => foods.json())
+//     .then(parsedFoods => {
+//         parsedFoods.forEach(food => {
+//             foodComponent = createFoodComponent(food)
+//             foodContainer.innerHTML += foodComponent
+//         })
+//     })
+
+    fetch("http://localhost:8088/food")
+    .then(response => response.json())
+    .then(myParsedFoods => {
+        myParsedFoods.forEach(food => {
+            console.log(food)
+            fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
+                .then(response => response.json())
+                .then(productInfo => {
+                    if (productInfo.product.ingredients_text) {
+                      food.ingredients = productInfo.product.ingredients_text
+                    } else {
+                      food.ingredients = "no ingredients listed"
+                    }
+                    foodComponent = createFoodComponent(food)
+                    foodContainer.innerHTML += foodComponent
+                })
         })
     })
-
